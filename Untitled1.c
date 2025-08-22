137e99d2381a82f096eaa129b8bbff75d5ac3ec6
khushbuha <khushbuha60@gmail.com>
2025-08-22 10:36:12 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>;
+void main()
+{
+    int a,b,c;
+    printf("enter two value");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+{
+    printf("a is greater");
+
+}
+else
+{
+    printf("b is greater");
+}
+}
