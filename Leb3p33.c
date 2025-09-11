21f596a226556f493cfae63156a450c911ecc03f
khushbuha <khushbuha60@gmail.com>
2025-09-11 20:22:34 +0530

    Create Leb3p33.c

---
+++ Leb3p33.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;  // बड़ा नंबर संभालने के लिए
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n < 0) {
+        printf("Factorial of negative numbers is not defined.\n");
+    } else {
+        for(i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu\n", n, fact);
+    }
+
+    return 0;
+}
