e637cb3f0c5caa4406840d8645f9c16ccfcd66b2
khushbuha <khushbuha60@gmail.com>
2025-09-26 10:53:31 +0530

    Add files via upload

---
+++ leb5.p6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+     char name[50];
+     printf("enter name\n" );
+
+     gets(name);
+
+{
+
+    int i;
+
+    for(i=0;name[i]!='\0';i++)
+    {
+
+
+         if(name[i]>='A'&&name[i]<='Z')
+         {
+             name[i]=name[i]+32;
+         }
+         else if(name[i]>='a'&&name[i]<='z')
+         {
+             name[i]=name[i]-32;
+         }
+    }
+             printf("name is %s\n",name);
+         }
+     }
