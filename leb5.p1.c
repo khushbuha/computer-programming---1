34430d47590ed7ad6f573274191aba5a055542a8
khushbuha <khushbuha60@gmail.com>
2025-09-26 09:57:55 +0530

    Add files via upload

---
+++ leb5.p1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char name[20];
+     printf("enter name\n" );
+    //scanf("%s",name);
+     //printf("name is %s",name);
+     gets(name);
+     printf("name is %s\n",name);
+}
